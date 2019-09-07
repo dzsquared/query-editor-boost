600c316aaf52352baab88f83e262997ccf17f704
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2019-09-07 17:29:06 -0500

    fixes #1

--- src/placescript.ts
+++ src/placescript.ts
@@ -4,18 +4,46 @@ import * as sqlops from 'azdata';
 import * as vscode from 'vscode';
 
 export class placeScript {
+
+    private connectionId: string = '';
+    private dbName: string = '';
+
     // places scriptText into fileName editor with current connection
-    public async placescript(scriptText) {
+    public async placescript(scriptText:string, context?:sqlops.IConnectionProfile, oecontext?: sqlops.ObjectExplorerContext) {
         try {
-            let connection = await sqlops.connection.getCurrentConnection();
+            var connection;
+            vscode.window.showInformationMessage('starting placescript');
+            if (context && context.id) {
+                this.connectionId = context.id;
+                this.dbName = context.databaseName;
+            } else if (oecontext) {
+                connection = oecontext.connectionProfile;
+                this.connectionId = connection.id;
+            } else {
+                connection = await sqlops.connection.getCurrentConnection();
+                if (connection) {
+                    this.connectionId = connection.connectionId;
+                    this.dbName = connection.databaseName;
+                }
+            }
             let doc = await vscode.workspace.openTextDocument({language: 'sql'});
             let editor = await vscode.window.showTextDocument(doc, 1, false);
             editor.edit(edit => {
                 edit.insert(new vscode.Position(0, 0), scriptText);
             });
-
-            if (connection.connectionId) {
-                await sqlops.queryeditor.connect(doc.uri.toString(), connection.connectionId);
+            if ((context || connection) && this.connectionId) {
+                if (this.dbName !== '') {
+                    var providerName:string;
+                    if (context) {
+                        providerName = context.providerName;
+                    } else if (connection) {
+                        providerName = connection.providerId;
+                    }
+                    let dProvider = await sqlops.dataprotocol.getProvider<sqlops.ConnectionProvider>(providerName, sqlops.DataProviderType.ConnectionProvider);            
+                    let connectionUri = await sqlops.connection.getUriForConnection(this.connectionId);
+                    await dProvider.changeDatabase(connectionUri,this.dbName);
+                }
+                await sqlops.queryeditor.connect(doc.uri.toString(), this.connectionId);
             }
         } catch (err) {
             vscode.window.showErrorMessage(err);
@@ -36,7 +64,7 @@ export class placeScript {
             editor.edit(edit => {
                 edit.insert(new vscode.Position(0, 0), scriptText);
             });
-            if (connection.connectionId) {
+            if (connection.connectionId) {  
                 await sqlops.queryeditor.connect(doc.uri.toString(), connection.connectionId);
             }
         } catch (err) {
